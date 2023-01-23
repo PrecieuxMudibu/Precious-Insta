95216d6c2142632deeecba55f2fe6759a774cfda
PrecieuxMudibu <oceangpmb@gmail.com>
2023-01-23 11:29:02 +0100

    refactor: Create component for items of menuList

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,20 @@ import instagram from '../../images/instagramText.png';
 import './home.css';
 import StatusHeaderCard from '../StatusHeaderCard/StatusHeaderCard';
 import PublicationCard from '../PublicationCard/PublicationCard';
-import { AiFillHome } from 'react-icons/ai';
-import { BsSearch, BsPersonCircle } from 'react-icons/bs';
-import { BiCompass } from 'react-icons/bi';
-import { FiSend } from 'react-icons/fi';
-import { FaRegHeart } from 'react-icons/fa';
-import { MdOutlineAddBox } from 'react-icons/md';
-import { GoThreeBars } from 'react-icons/go';
 import { Link } from 'react';
+import MenuLink from '../MenuLink/MenuLink';
 export default function Home() {
   let repeat = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+  const menuList = [
+    'Accueil',
+    'Recherche',
+    'Découvrir',
+    'Messages',
+    'Notifications',
+    'Créer',
+    'Profil',
+    'Plus',
+  ];
   return (
     <div className="home">
       <div className="home__left">
@@ -19,38 +23,9 @@ export default function Home() {
           <img src={instagram} alt="" />
         </div>
         <ul>
-          <li className="home__link">
-            <AiFillHome className="home__link-icon" />
-            <span>Accueil</span>
-          </li>
-          <li className="home__link">
-            <BsSearch className="home__link-icon" />
-            <span>Recherche</span>
-          </li>
-          <li className="home__link">
-            <BiCompass className="home__link-icon" />
-            <span>Découvrir</span>
-          </li>
-          <li className="home__link">
-            <FiSend className="home__link-icon" />
-            <span>Messages</span>
-          </li>
-          <li className="home__link">
-            <FaRegHeart className="home__link-icon" />
-            <span>Notifications</span>
-          </li>
-          <li className="home__link">
-            <MdOutlineAddBox className="home__link-icon" />
-            <span>Créer</span>
-          </li>
-          <li className="home__link">
-            <BsPersonCircle className="home__link-icon" />
-            <span>Profil</span>
-          </li>
-          <li className="home__link">
-            <GoThreeBars className="home__link-icon" />
-            <span>Plus</span>
-          </li>
+          {menuList.map((element) => (
+            <MenuLink text={element} />
+          ))}
         </ul>
       </div>
 
@@ -83,7 +58,7 @@ export default function Home() {
               <p>Suggestions pour vous</p>
               <button>Voir tout</button>
             </div>
-            
+
             <div>
               <ul>
                 <li className="suggested-contacts">
--- src/components/StatusHeaderCard/StatusHeaderCard.jsx
+++ src/components/StatusHeaderCard/StatusHeaderCard.jsx
@@ -1,8 +1,10 @@
 import instagram from '../../images/instagramText.png';
-import './statusHeaderCard.css'
-export default function StatusHeaderCard (){
-    return (<div className="home__status">
-    <img src={instagram} alt="" />
-    <p>Keke__</p>
-  </div>)
+import './statusHeaderCard.css';
+export default function StatusHeaderCard() {
+  return (
+    <div className="home__status">
+      <img src={instagram} alt="" />
+      <p>Keke__</p>
+    </div>
+  );
 }
