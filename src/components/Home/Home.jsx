167cf8579b43db93e8b88d35b1173d7a0f78ea85
PrecieuxMudibu <oceangpmb@gmail.com>
2023-01-23 19:16:09 +0100

    feat: Finished tablette and mobile views

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,8 @@ import StatusHeaderCard from '../StatusHeaderCard/StatusHeaderCard';
 import PublicationCard from '../PublicationCard/PublicationCard';
 import { Link } from 'react-router-dom';
 import MenuLink from '../MenuLink/MenuLink';
+import { useEffect } from 'react';
+import { BsInstagram } from 'react-icons/bs';
 export default function Home() {
   let repeat = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
   const menuList = [
@@ -17,10 +19,24 @@ export default function Home() {
     'Profil',
     'Plus',
   ];
+
+  async function fetchData () {
+    await fetch('https://images.ctfassets.net')
+      .then((response) => response.json())
+      .then((response) => alert(JSON.stringify(response)))
+      .then((response) => console.log(response))
+      .catch((error) => alert('Erreur : ' + error));
+  }
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, [repeat]);
+
   return (
     <div className="home">
       <div className="home__left">
         <div className="home__logo">
+          <BsInstagram className="home__link-icon instagram"/>
           <img src={instagram} alt="" />
         </div>
         <ul>
@@ -73,21 +89,37 @@ export default function Home() {
           </div>
 
           <div className="current-user__links">
-            <Link className="small-text">A propos<span> ● </span></Link>
+            <Link className="small-text">
+              A propos<span> ● </span>
+            </Link>
 
-            <Link className="small-text">Aide<span> ● </span></Link>
+            <Link className="small-text">
+              Aide<span> ● </span>
+            </Link>
 
-            <Link className="small-text">Presse<span> ● </span></Link>
+            <Link className="small-text">
+              Presse<span> ● </span>
+            </Link>
 
-            <Link className="small-text">API<span> ● </span></Link>
+            <Link className="small-text">
+              API<span> ● </span>
+            </Link>
 
-            <Link className="small-text">Emplois<span> ● </span></Link>
+            <Link className="small-text">
+              Emplois<span> ● </span>
+            </Link>
 
-            <Link className="small-text">Confidentialité<span> ● </span></Link>
+            <Link className="small-text">
+              Confidentialité<span> ● </span>
+            </Link>
 
-            <Link className="small-text">Conditions<span> ● </span></Link>
+            <Link className="small-text">
+              Conditions<span> ● </span>
+            </Link>
 
-            <Link className="small-text">Lieux<span> ● </span></Link>
+            <Link className="small-text">
+              Lieux<span> ● </span>
+            </Link>
 
             <Link className="small-text">Langue</Link>
           </div>
